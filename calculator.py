1168c116577deb9e19c0020f462a10fa705991e8
nouf <noufalrashdii@outlook.com>
2025-05-18 11:22:49 +0400

    second

---
+++ calculator.py
@@ -0,0 +1,10 @@
+x = int(input(" enter any number"))
+y = int(input("enter any number"))
+A = x + y
+S = x - y
+M = x * y
+D = x / y
+print("substraction:" , S)
+print("Multiplication:" , M)
+print("Addition:" , A)
+print("Division:" , D)
